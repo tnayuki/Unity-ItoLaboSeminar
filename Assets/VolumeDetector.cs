6f54c575c07b15dbc0eb8a9514522b4288b221b8
Toru Nayuki <tnayuki@icloud.com>
2015-10-20 18:29:37 +0900

    ボリューム検出の実装

---
+++ Assets/VolumeDetector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeDetector : MonoBehaviour {
+	public float volume;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnAudioFilterRead(float[] data, int channels)
+	{
+		float total = 0.0f;
+		for (int i = 0; i < data.Length; i++) {
+			total += Mathf.Abs (data [i]);
+		}
+		total /= data.Length;
+		
+		volume = total;
+	}
+}

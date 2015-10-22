e44e63176a2418a7e8f70edd18115815e30cc84e
Toru Nayuki <tnayuki@icloud.com>
2015-10-22 15:56:58 +0900

    マイク入力対応

---
+++ Assets/MicrophoneStarter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneStarter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioSource audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = Microphone.Start(null, true, 10, 44100);
+		audioSource.Play ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
